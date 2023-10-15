8ff7918a79c198a0588a441e9a82991db23f3f86
unkn0t <warriorkesh4@gmail.com>
2023-10-15 14:20:54 +0600

    add nvim

---
+++ .config/nvim/lua/custom/plugins/rust-tools.lua
@@ -0,0 +1,22 @@
+return {
+  "simrat39/rust-tools.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    -- Debugging
+    "nvim-lua/plenary.nvim",
+    "mfussenegger/nvim-dap"
+  },
+  config = function ()
+    local rt = require("rust-tools")
+    rt.setup({
+      server = {
+        on_attach = function(_, bufnr)
+        -- Hover actions
+        vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+        -- Code action groups
+        vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+        end,
+      },
+    })
+  end,
+}
